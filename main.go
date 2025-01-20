f0d81944bd1e6fba60d4c32aed0176c01a369cf1
Caaki <nikola.lazin.4384@metropolitan.ac.rs>
2025-01-20 15:44:37 +0100

    Creating project that depicts how ray tracing woriks using go lang

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/canvas"
+)
+
+func main() {
+
+	myApp := app.New()
+	w := myApp.NewWindow("Circle")
+
+	circle := canvas.NewCircle(color.White)
+	circle.StrokeColor = color.Gray{Y: 0x99}
+	circle.StrokeWidth = 5
+	w.SetContent(circle)
+
+	w.Resize(fyne.NewSize(100, 100))
+	w.ShowAndRun()
+
+}
